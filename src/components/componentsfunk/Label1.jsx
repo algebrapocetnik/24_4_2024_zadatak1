15975d989875795a2e87d7d3d0d15b3b5c0ffd24
Marsel Hajdinjak <marsel.hajdinjak@gmail.com>
2024-04-26 18:01:49 +0200

    zad

---
+++ src/components/componentsfunk/Label1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Label1() {
+  const Text = "Ovo je tekst u funkcijskoj komponenti.";
+    return (
+      <div>
+        <p>{Text}</p>
+      </div>
+    );
+  };
+
+
+export default Label1;
---
+++ src/components/componentsklass/Label.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Label extends React.Component {
+  render() {
+    return <p>Vježbam klasnu komponentu</p>;
+  }
+}
